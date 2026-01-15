7bbc2cc3592ae79a20bd39329ac2659291a782f5
Bruno Salles <brunobr2603salles@gmail.com>
2026-01-14 21:14:07 -0300

    Criado a Controller

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,19 @@
+package dev.java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // um controlador para API rest - ira mapear o arquivo
+@RequestMapping // anda junto com a RestController -serve para colocar todas as outras rodas no mesmo lugar
+public class Controller {
+
+
+    @GetMapping("/boasVindas") // pega informações
+    public String boasVindas(){
+        return "Essa e minha primeira mensagem nessa rota";
+    }
+
+    // todos que acessarem a minha rota ira ver esta mensagem
+
+}
