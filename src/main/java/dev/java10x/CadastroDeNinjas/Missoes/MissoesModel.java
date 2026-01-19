d7840b2a7b26d44fc0a826b6ba456d5bdc8c4c42
Bruno Salles <brunobr2603salles@gmail.com>
2026-01-19 16:14:22 -0300

    Modelagem do Banco De Dados, NinjaMolde e MissoesModel Concluidos, anotations realizadas para o relacionamento de tabelas

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -0,0 +1,24 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjasModel;
+import jakarta.persistence.*;
+
+@Entity // rudo o que esta abaixo ate ter um colchete ou ponto e virgula e uma entidade
+@Table (name = "tb_Missoes")
+public class MissoesModel {
+
+    // relacionamento entre tabelas uma missao pode ter varios ninjas mais um ninja pode ter apenas uma missao por vez
+    // como atribui a um unico ninja a uma missao e como atribui uma missao a varios ninjas
+
+    // tabela
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String difuldade;
+
+   // @OneToMany - uma missao pode ter varios ninjas
+    @OneToMany(mappedBy = "missoes") // toda vez que realizar um relacionamento entre duas tabelas e necessario mappear e conectar atraves de uma chave estrangeira
+    private NinjasModel ninja;
+
+}
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjasModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjasModel.java
@@ -1,24 +1,39 @@
-package dev.java10x.CadastroDeNinjas;
+package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 // para trabalhar com banco de dados e necessario baixar uma dependencia chamada Springjpa no initializr para ultilizar uma anotação chamada @Entity
 @Entity // com o Entity minha classe vira uma entidade do BD
 @Table(name = "tb_cadastro") // com o @Entity se criou uma tabela com o @Table coloca um nome na tabela
 // JPA - java persistence API
-public class NinjaModel {
+public class NinjasModel {
 
     @Id // por isso e necessario passar esta anotação
     @GeneratedValue(strategy = GenerationType.IDENTITY) // para complementar dever ser passodo uma, estrategia de como vai ser ultilizado este ID com o @GeneratedValues
-    Long id; // algo que apenas e declarado, pois o java ja coloca o id automaticamente apenas sendo necessario passar como deve ser feito
+    private Long id; // algo que apenas e declarado, pois o java ja coloca o id automaticamente apenas sendo necessario passar como deve ser feito
+
     private String nome;
+
     private String email;
+
     private int idade;
 
-    public NinjaModel() {
+    //@ManyToOne - Um ninja tem uma unica missao
+    @ManyToOne
+    // Foreing Key ou chave estrangeira - se no @OneToMany e necessario mapear, com o JoinColumn ira juntar as duas colunas e isso ira criar outra coluna no banco de dados
+    // toda vez que clicar na chave estrange missoes_id ira pegar informaçoes das duas tabelas
+    @JoinColumn(name = "missoes_id")
+    private List<MissoesModel> missoes; // nova coluna da tabela
+
+
+
+    public NinjasModel() {
     }
 
-    public NinjaModel(String nome, String email, int idade) {
+    public NinjasModel(String nome, String email, int idade) {
         this.nome = nome;
         this.email = email;
         this.idade = idade;
